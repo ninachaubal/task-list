c7936247b0e3a4f140f34c830231172335d02a07
Nina Chaubal <ninachaubal@gmail.com>
2018-05-10 19:17:11 -0700

    Generated Build

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import tasks from './tasks'
-import visibleGroup from './visibleGroup'
-
-export default combineReducers({
-  tasks,
-  visibleGroup
-})
--- src/reducers/visibleGroup.js
+++
@@ -1,6 +0,0 @@
-import ALL_TASKS from '../constants/AllTasks'
-import ActionTypes from '../constants/ActionTypes'
-// Updates the visibleGroup in our state in case of a 'SHOW_TASK_GROUP' action.
-export default function(state = ALL_TASKS, action) {
-  return action.type === ActionTypes.SHOW_TASK_GROUP ? action.name : state
-}
